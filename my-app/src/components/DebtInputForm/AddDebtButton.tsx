8c90323f3bf5997af6f0de997c055e5f486bc86a
Nicholas Ewing <nicholasSewing@gmail.com>
2023-01-04 09:44:03 -0500

    Renames onClick to handleNewItem in <AddDebtButton />

--- my-app/src/components/DebtInputForm/AddDebtButton.tsx
+++ my-app/src/components/DebtInputForm/AddDebtButton.tsx
@@ -3,12 +3,12 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { MouseEventHandler } from "react";
 
 interface AddDebtButtonProps {
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  handleNewItem: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function AddDebtButton({ onClick }: AddDebtButtonProps) {
+export default function AddDebtButton({ handleNewItem }: AddDebtButtonProps) {
   return (
-    <button onClick={onClick} className="space-x-2 text-sm-blue">
+    <button onClick={handleNewItem} className="space-x-2 text-sm-blue">
       <FontAwesomeIcon className="stroke-current stroke-[60px]" icon={faPlus} />
       <span>Add Another Debt</span>
     </button>
