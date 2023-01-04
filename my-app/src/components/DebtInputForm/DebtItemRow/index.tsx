4f93723f8d72bf69a05ddae92930d755862e8042
Nicholas Ewing <nicholasSewing@gmail.com>
2023-01-04 11:03:57 -0500

    Adds and styles remove debt item button

--- my-app/src/components/DebtInputForm/DebtItemRow/index.tsx
+++ my-app/src/components/DebtInputForm/DebtItemRow/index.tsx
@@ -1,14 +1,23 @@
 import Input from "./Input";
-import DebtItem from "../../../types/DebtItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Row from "../../Containers/Row";
+import { DebtItem } from "../../../types";
+
+interface DebtItemRowProps extends DebtItem {
+  handleRemoveItem: (id: number) => void;
+}
 
 export default function DebtItemRow({
+  id,
   name,
   remainingAmount,
   currentApr,
   currentMonthly,
-}: DebtItem) {
+  handleRemoveItem,
+}: DebtItemRowProps) {
   return (
-    <>
+    <Row>
       <Input type="text" name="debt-name" placeholder="Medical" />
       <Input
         type="number"
@@ -29,6 +38,17 @@ export default function DebtItemRow({
         placeholder="200"
         symbol="$"
       />
-    </>
+      {id > 0 && (
+        <button
+          onClick={() => handleRemoveItem(id)}
+          className="absolute -right-6 top-1/3"
+        >
+          <FontAwesomeIcon
+            className=" rotate-45 stroke-current stroke-[60px] text-lg text-sm-gray"
+            icon={faPlus}
+          />
+        </button>
+      )}
+    </Row>
   );
 }
