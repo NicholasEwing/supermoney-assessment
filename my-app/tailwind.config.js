da3e833c640e197345c48bef4c183e464ca0e658
Nicholas Ewing <nicholasSewing@gmail.com>
2023-01-04 07:48:46 -0500

    Updates configs: gitignore, package.json, prettier / tailwind

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sm-black": "#0D0B2D",
+        "sm-dark-gray": "#504F54",
+        "sm-light-gray": "#C9C9C9",
+        "sm-blue": "#06A9DB",
+        sky: "#F4FBFD",
+      },
+      fontFamily: {
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
