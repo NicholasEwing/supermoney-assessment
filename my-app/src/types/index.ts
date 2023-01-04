9a79d318dc28b88b49d2e49a091d41176d358753
Nicholas Ewing <nicholasSewing@gmail.com>
2023-01-04 11:03:38 -0500

    Adds DebtItems to types

--- my-app/src/types/index.ts
+++ my-app/src/types/index.ts
@@ -1,6 +1,9 @@
-export default interface DebtItem {
+export interface DebtItem {
+  id: number;
   name: string;
   remainingAmount: number;
   currentApr: number;
   currentMonthly: number;
 }
+
+export type DebtItems = DebtItem[];
